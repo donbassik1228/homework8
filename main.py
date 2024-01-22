e885ccaa1ed60b54964a0b7b267fbc4f32d43534
donbassik1228 <148227783+donbassik1228@users.noreply.github.com>
2024-01-22 16:19:03 +0200

    fixed code

--- main.py
+++ main.py
@@ -1,54 +1,48 @@
-from datetime import date, timedelta
-
+from datetime import datetime, timedelta, date
 def get_birthdays_per_week(users):
+    birthdays_per_week = {}
     today = date.today()
-    current_weekday = today.weekday()
-
-    
-    monday = today - timedelta(days=current_weekday)
-
-    
-    birthdays_per_week = {
-        'Monday': [],
-        'Tuesday': [],
-        'Wednesday': [],
-        'Thursday': [],
-        'Friday': [],
-        'Saturday': [],
-        'Sunday': [],
-    }
-
     for user in users:
-        user_name = user['name']
-        user_birthday = user['birthday'].replace(year=today.year)
-
+        name = user["name"]
+        birthday = user["birthday"]
         
-        if user_birthday < today:
-            user_birthday = user_birthday.replace(year=today.year + 1)
-
+        birthday_this_year = datetime(
+            today.year, birthday.month, birthday.day
+            ).date()
+        if today > birthday_this_year:
+            
+            birthday_next_year = datetime(
+                today.year + 1, birthday.month, birthday.day
+                ).date()
+        else:
+            
+            birthday_next_year = birthday_this_year
         
-        if today <= user_birthday <= monday + timedelta(days=6):
-            
-            birthday_weekday = (user_birthday - monday).days
-
-            
-            if birthday_weekday == 0 and user_birthday < today:
-                birthday_weekday = 7
-
-            
-            birthdays_per_week[list(birthdays_per_week.keys())[birthday_weekday - 1]].append(user_name)
-
+        if birthday_next_year.weekday() == 5:  
+            birthday_next_year += timedelta(days=2)
+        elif birthday_next_year.weekday() == 6:  
+            birthday_next_year += timedelta(days=1)
+        
+        time_left_until_bd = birthday_next_year - today
+        
+        days_left = time_left_until_bd.days
+        if 0 <= days_left <= 7:
+            day_of_week = birthday_next_year.strftime('%A')
+            if day_of_week not in birthdays_per_week:
+                birthdays_per_week[day_of_week] = [name.split()[0]]
+            else:
+                birthdays_per_week[day_of_week].append(name.split()[0])
     return birthdays_per_week
-
-
 if __name__ == "__main__":
     users = [
-        {"name": "Maria", "birthday": date(2024, 1, 15)},
-        {"name": "Oleg", "birthday": date(2024, 1, 16)},
-        {"name": "Bogdan", "birthday": date(2024, 1, 18)},
-        {"name": "Vlad", "birthday": date(2024, 1, 20)},
-        {"name": "Sergio", "birthday": date(2024, 1, 22)},
+        {"name": "Bogdan", "birthday": datetime(2004, 12, 24).date()},
+        {"name": "Maria", "birthday": datetime(2006, 6, 30).date()},
+        {"name": "Sergiy", "birthday": datetime(2005, 7, 15).date()},
+        {"name": "Martin", "birthday": datetime(2003, 3, 12).date()},
+        {"name": "Vlad", "birthday": datetime(1999, 2, 7).date()},
     ]
-
     result = get_birthdays_per_week(users)
     print(result)
+    
+    for day_name, names in result.items():
+        print(f"{day_name}: {', '.join(names)}")
